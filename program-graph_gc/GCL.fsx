d872eb0a4b5cadc0dafe235ecf0a24ff44fb55ae
JulienLacour98 <julienlacour98@gmail.com>
2021-10-25 16:54:36 +0200

    Update RD

--- program-graph_gc/GCL.fsx
+++ program-graph_gc/GCL.fsx
@@ -26,22 +26,7 @@ let parse input =
     // return the result of parsing
     res
 
-(*
-// prints the grafical string representation of a (set) pg
-let rec printSet s = Set.iter (fun (qI,st,qF) -> printfn "q%d -> q%d [label = \"%s\"];" qI qF st) s
-let printGraphviz s = printfn "\nGraphviz:"
-                      printfn  "digraph program_graph {rankdir=LR; \nnode [shape = circle]; q0; \nnode [shape = doublecircle]; q00; \nnode [shape = circle] "
-                      printSet s
-                      printfn "}"
-*)
-
-(*let rec isDone e = 
-    match e with 
-    | WhileStat(b,s) -> (NegExpr b)
-    //| ElseIfExpr(GC1,GC2) -> (AndExpr (isDone GC1, isDone GC2))
-    | _ -> failwith "no matchcase for input isDone"
-*)
-
+   
 type Action = 
     | Program of program
     | Declaration of decl
@@ -59,7 +44,7 @@ let rec edges qI qF action =
 
 and edgesP qI qF p = 
     match p with 
-    | Prog(d,s) -> fresh <- fresh + 1
+    | Prog(d,s) -> fresh <- fresh
                    let q = fresh
                    let e1 = edgesD qI q d
                    let e2 = edgesS q qF s
@@ -111,71 +96,28 @@ and edgesS qI qF s =
     | Read(l)               -> set [qI, Statement (s), qF]
     | Write(x)              -> set [qI, Statement (s), qF]
 
-// test: 
- //edges 0 1 (IfElseStat((GrThan(Var "x", Num 0)), (Assign(Var "x", Num 2)),(Assign(Var "y", Num 2)))) 
-// edges "▷" "◀" (Stats((Ass(Variable "x", Num 2.0)),(Ass(Variable "y", Num 2.0))))
-
- 
-(*
-// not done !!!
-(*
-let rec fv a =
-    match a with
-    | Variable(x)            -> Set [x]
-    | Num(n)                 -> Set.empty
-    //| Array(a1,a2)           -> 
-    | PlusExpr(a1,a2)         
-    | MinusExpr(a1,a2)        
-    | ModExpr(a1,a2) 
-    | TimesExpr(a1,a2)       
-    | DivExpr(a1,a2)         -> Set.union (fv a1) (fv a2)  
-    | UMinusExpr(a0)         -> fv a0
-    | True 
-    | False                  -> Set.empty
-    | AndExpr(b1,b2)          
-    | OrExpr(b1,b2)          -> Set.union (fv b1) (fv b2) 
-    | NegExpr(b0)            -> fv b0
-    | Equals(a1,a2)       
-    | NotEquals(a1,a2)     
-    | GrThan(a1,a2)       
-    | GrEqThan(a1,a2)  
-    | LeThan(a1,a2)         
-    | LeEqThan(a1,a2)        -> Set.union (fv a1) (fv a2)   
-    |_ -> failwith "error in fv"
-
-//fvb (LeThan (Variable "y", Variable "x"))
-
-// not done see p 27
-let rec killLV e =
-    match e with 
-    | Ass(x,a) -> fv x 
-    |_ -> Set.empty
-
-// not done see p 27
-let rec genLV e =
-    match e with 
-    | Ass(x,a) -> fv a 
-    | e -> fv e
-
-    //| _ -> Set.empty
- 
-let e1 = Ass (Variable "x", Num 2)
-let e2 = NegExpr (GrThan (Variable "x", Num 0))
-let e3 = GrThan (Variable "x", Num 0)
-let e4 = Ass (Variable "x", MinusExpr (Variable "x", Num 1))
-
-killLV (Ass (Variable "r", MinusExpr (Variable "r", Variable "y")))
-genLV (Ass (Variable "r", MinusExpr (Variable "r", Variable "y")))
-
-killLV e3
-genLV e3
-*)
-
-// TODO - Create this function to get variable in assignments
 let def alpha = 
     match alpha with 
-    | Ass(Variable(x),_) -> x
-    | _ -> ""
+    | Declaration(d)    -> match d with 
+                            | VariableDeclaration(x)    -> x
+                            | ArrayDeclaration(_,A)     -> A
+                            | RecordDeclaration(R)      -> R
+                            | _                         -> "" 
+    | Statement(s)      -> match s with 
+                            | AssX(x, _)    -> x
+                            | Ass(l, _)     -> match l with 
+                                                | VariableL(x) -> x
+                                                | ArrayExpressionL(A,_) -> A
+                                                | FirstRecordL(R) -> R
+                                                | SecondRecordL(R) -> R
+                            | RecordAss(R, _, _) -> R
+                            | Read(l)            -> match l with 
+                                                    | VariableL(x) -> x
+                                                    | ArrayExpressionL(A,_) -> A
+                                                    | FirstRecordL(R) -> R
+                                                    | SecondRecordL(R) -> R
+                            | _         -> ""
+    | _                 -> ""
 
 let getNodes edgesSet = 
     let mutable nodes = Set.empty
@@ -196,6 +138,8 @@ let reachingDefinitions edges =
     for variable in variables do 
         if (variable <> "") then 
             rd.[0] <- rd.[0].Add(variable, -1, 0)
+    printfn "V:\n%A" variables
+    printfn "V:\n%A" rd
     let mutable over = false
     while not over do 
         let mutable newRd = false
@@ -215,9 +159,8 @@ let reachingDefinitions edges =
             over <- true
     rd
 
-//y:= 1; x:=2;  while (x<=100) { if (y <10) { y := y +1; } else {x := x +10;}}
+//{y:= 1; x:=2;  while (x<=100) { if (y <10) { y := y +1; } else {x := x +10;}}}
 //printfn "%A" (reachingDefinitions (set [(0, "y", 1); (1, "x", 2); (2, "", 3); (3, "", 4); (4, "y", 2); (3, "", 5); (5, "x", 2); (2, "", 6)]))
-*)
 
 //function that takes input from user and prints corresponding graphviz file if the given string has valid GCL syntax
 // and gets an error otherwise
@@ -231,9 +174,9 @@ let rec compute n =
         printfn "HELOOO"
         printfn "AST:\n%A" ast
 
-        let pg = (edges 0 6 (Program ast))
+        let pg = (edges 0 5 (Program ast))
         printfn "PG:\n%A" pg
-        //printfn "RD:\n%A" (reachingDefinitions pg)
+        printfn "RD:\n%A" (reachingDefinitions pg)
         //let pg = (edges 0 -1 ast) 
         //printfn "PG:\n%A" pg
         //printGraphviz pg
@@ -241,7 +184,5 @@ let rec compute n =
         //fresh <- 0
         compute n 
 compute 3
-
-//getNodes (set [(0, "y := 1", 2); (2, "!(x>0)", 1); (2, "x>0", 3); (3, "y := y*x", 4); (4, "x := x-1", 2)])
 
 // {int x; int[3] A; {int fst; int snd} R; while (not x == 3) {x := 3 + 5;}}
